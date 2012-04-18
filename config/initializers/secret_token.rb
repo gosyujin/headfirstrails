011b3a0ef2b164bb538767ba4219fae09cabebc5
kk_Ataka <kk_ataka@example.com>
2012-04-18 12:14:13 +0900

    Generate headfirstrails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Headfirstrails::Application.config.secret_token = '9d80be9be3f8f2c6e071bca295015de391f34d83e04bd85284e1e8224c16140f0cb7d026037224028c924cd415b62dc6f00d42cc0bbe4024d5ad7e703ecae192'
